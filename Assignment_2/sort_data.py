34a46c3f551c7cb858f998df19d63f9ca8d1e77c
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2021-04-13 10:15:12 +0200

    Add files via upload

---
+++ Assignment_2/sort_data.py
@@ -0,0 +1,22 @@
+profits = list()
+
+encoding = input(
+    "Select type of encoding:\n1. Binary encoding. [Default]\n2. Integer encoding.\n> ")
+text_name = input("\nSelect text file:\n> ")
+
+file_name = ""
+
+if encoding == "2":
+    file_name = "./integer_encoding_data/" + text_name
+else:
+    file_name = "./binary_encoding_data/" + text_name
+
+with open(file_name) as fin:
+    for line in fin:
+        profits.append(line.strip())
+
+profits.sort()
+
+with open(file_name, 'w') as fout:
+    for profit in profits:
+        fout.write(profit + '\n')
