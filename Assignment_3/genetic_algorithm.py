3b9d4e01e337fb72705f9bffa6d34036d227c53e
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2021-05-08 17:59:34 +0200

    Add files via upload

---
+++ Assignment_3/genetic_algorithm.py
@@ -0,0 +1,111 @@
+import random
+
+
+def mutation_1(constraint, largest_record_length):
+    # Adds a new random event to the current constraint if the length of the current constraint is smaller than the maximum one on the data base.
+    events = ['A', 'B', 'C', 'D', 'E']
+
+    if len(constraint) < largest_record_length:
+        keys = list(constraint.keys())
+        values = list(constraint.values())
+        # contraint[last_event + new_random_event].append([last_time, random_time(last_time, last_time + 10)])
+        constraint[keys[-1][-1] + events[random.randint(0, 4)]].append([values[-1][0][1], random.randint(int(
+            values[-1][0][1]), int(values[-1][0][1]) + 5)])
+
+
+def sortPopulationByProfit(p):
+    #	Sorting handling function.
+    return p[1]
+
+
+def evaluateElite(elite, elite_threshold):
+    #	Evaluate the current elite.
+    elite.sort(key=sortPopulationByProfit, reverse=True)
+    counter = len(elite)
+    i = 0
+
+    while i < counter:
+        j = i + 1
+        while j < counter:
+            if elite[i] == elite[j]:
+                elite.pop(j)
+                counter -= 1
+                j -= 1
+            j += 1
+        i += 1
+
+    while len(elite) > elite_threshold:
+        elite.pop(-1)
+
+    return elite
+
+
+def saveEliteIntoDictionary(dictionary, elite):
+    # Saves the current elite into dictionary data structure.
+    for i in range(len(elite)):
+        dictionary['Elite'].append(elite[i][0])
+        dictionary['Profit'].append(elite[i][1])
+        dictionary['Graph'].append(elite[i][2])
+
+
+def selectElite(population, elite, elite_threshold):
+    # Selects the best "elite_threshold" elements of the populations and stores it in "elite".
+    population.sort(key=sortPopulationByProfit, reverse=True)
+    if len(population) != 0:
+        for i in range(elite_threshold):
+            elite.append(population[i])
+
+
+def applyGeneticOperator(nConstraints, mProb, largest_record_length):
+    # new_population will store --> [Indexes of solution / % of satisfaction / Graph]
+    new_nConstraints = []
+    parents = []
+
+    while len(new_nConstraints) != len(nConstraints):
+        #	Select 2 random parents from the population.
+        parents = select2Parents(nConstraints).copy()
+
+        #	Mutate parents with a probability mProb
+        #		if random.randint(1,100) <= mProb:
+        #			Example:
+        #			{'AB': [['1', '3']], 'BA': [['3', '4']]} --> {'BA': [['3', '4']], 'AA': [['1', '3']]}
+        if random.randint(1, 100) <= mProb * 100:
+            mutateParents(parents)
+
+        for i in range(len(parents)):
+            new_nConstraints.append(parents[i])
+
+    return new_nConstraints
+
+
+def mutateParents(p):
+    # Mutate a random event from each parent.
+    events = ['A', 'B', 'C', 'D', 'E']
+    to_mutate = events[random.randint(0, 4)]
+    new_mutation = to_mutate
+    while new_mutation == to_mutate:
+        new_mutation = events[random.randint(0, 4)]
+
+    for i in range(len(p)):
+        for j in list(p[i].keys()):
+            current_key = j
+            if j[0] == to_mutate:
+                new_key = new_mutation + j[1]
+                if current_key in p[i]:
+                    p[i][new_key] = p[i].pop(current_key)
+                    current_key = new_key
+            if j[1] == to_mutate:
+                new_key = j[0] + new_mutation
+                if current_key in p[i]:
+                    p[i][new_key] = p[i].pop(current_key)
+
+
+def select2Parents(p):
+    #	Selects 2 parents.
+    parents = []
+    i = 0
+    while i < 2:
+        parents.append(random.choice(p))
+        i += 1
+
+    return parents
---
+++ Assignment_3/main.py
@@ -0,0 +1,106 @@
+import sys
+import copy
+import pandas as pd
+import time
+
+import genetic_algorithm
+import graph
+import data_management
+
+# Network graphs: https://www.youtube.com/watch?v=9aZiwuQTo-4
+
+
+def main():
+    if len(sys.argv) == 2:
+        records = {'Event': [], 'Time': []}
+        lines = []
+        nConstraints = []
+
+        # Population size [20 - 50].
+        #   Note: Even number.
+        nSolutions = 20
+        if nSolutions % 2 != 0:
+            nSolutions += 1
+
+        # Mutation probability.
+        #   Note: In order to avoid randomness -->  [mProb <= 0.2].
+        mProb = 0.2
+
+        # Gets the data into "lines".
+        data_management.importFromTXTFile(sys.argv[1], lines)
+
+        # Splits among "Events" and "Times" and stores it in "records" dictionary.
+        data_management.moveToRecords(records, lines)
+
+        # Generates a dataframe with the previous data.
+        records_df = data_management.createDataFrame(records)
+
+        # Gets the length of the largest record. This will be used later in mutation.
+        largest_record_length = data_management.getLargestRecords(
+            records_df['Event'])
+
+        # Gets a constraint similar to one of the records or generate new one.
+        graph.obtainConstraints(records_df, nConstraints, nSolutions)
+        # print("Constraints:", nConstraints)
+
+        elite = []
+        # Population will store --> [Indexes of solution / % of satisfaction / Graph]
+        population = []
+        elite_threshold = 3
+
+        dictionary = {'Elite': [], 'Profit': [], 'Graph': []}
+
+        start_time = time.time()
+
+        for constraint in nConstraints:
+            s = graph.evaluateGraph(records_df, constraint)
+
+            # Appends into population --> indexes of the records that satisfy the constraints and the satisfaction percentage of the total elements.
+            population.append([s, len(s)/len(records_df['Event']), constraint])
+
+        # Selects the best parents of the very first population and saves them into elite.
+        genetic_algorithm.selectElite(population, elite, elite_threshold)
+
+        # Number of generations [100 - 300].
+        maxGenerations = 20
+
+        iterations = 1
+        while iterations <= maxGenerations:
+            nSolutions = genetic_algorithm.applyGeneticOperator(
+                nConstraints, mProb, largest_record_length)
+
+            population = []
+            for constraint in nSolutions:
+                s = graph.evaluateGraph(records_df, constraint)
+                population.append(
+                    [s, len(s)/len(records_df['Event']), constraint])
+
+            # Selects the best parents and saves them into elite.
+            genetic_algorithm.selectElite(population, elite, elite_threshold)
+
+            # Checks the elite and take the unique elements.
+            elite = genetic_algorithm.evaluateElite(
+                elite, elite_threshold).copy()
+
+            iterations += 1
+
+        seconds = (time.time() - start_time)
+
+        # Saves elite into dictionary.
+        genetic_algorithm.saveEliteIntoDictionary(dictionary, elite)
+
+        # Creates elite dataframe.
+        elite_df = pd.DataFrame(dictionary, columns=[
+                                'Elite', 'Profit', 'Graph'])
+
+        #elite_df.to_excel("test.xlsx", index=False, header=True)
+        print(elite_df)
+
+        print("\n%s seconds" % seconds)
+
+    else:
+        print("Mistaken input!\nExample: python3 main.py <data_base>.txt")
+
+
+if __name__ == "__main__":
+    main()
